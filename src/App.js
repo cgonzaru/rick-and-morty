fda62d405d89b1711889a632c04e17d86ccc6890
cgonzaru <cristina.glez.rubio@gmail.com>
2019-12-16 11:36:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app__header">
+        <h1 className="header__title">Rick and Morty</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
