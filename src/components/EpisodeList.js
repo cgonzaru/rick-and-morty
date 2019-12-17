23655a9887e38262f3702b9679a5ce1d0fb00278
cgonzaru <cristina.glez.rubio@gmail.com>
2019-12-17 12:06:46 +0100

    added logo and images

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import temporada1 from '../images/temporada1.jpg';
+import temporada2 from '../images/temporada2.jpg';
 //import EpisodeCard from './EpisodeCard';
 
 const EpisodeList = props => {
@@ -8,12 +10,22 @@ const EpisodeList = props => {
 		<div className="episodes-list row">
 			{episodes
 				.map(item => {
+					const temp = item.episode.split("E");
+					let t = '';
+					if(temp[0] === 'S01') {
+						t = temporada1;
+					} else {
+						t = temporada2;
+					}
 					return (
 						<div className="episode-name col s4" key={item.id}>
 
 							<div className="card blue">
+								<div className="card-image">
+									<img src={t} alt="logot1" className="logo"/>
+									<span className="card-title black-text">{item.episode}</span>
+								</div> 
 								<div className="card-content">
-									<span className="card-title">{item.episode}</span>
 									<p className="episode-name">{item.name}</p>
 								</div>
 							</div>
