2f3a91b5b7ceb1adf0a39d11564c41ec9e33f846
cgonzaru <cristina.glez.rubio@gmail.com>
2019-12-18 22:47:54 +0100

    generate docs

---
+++ docs/precache-manifest.53650975fe19757ad55841dbc7165e37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e0e41e0814a1fc6ac77c901304ee04e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8bce9338540b21d6557",
+    "url": "./static/css/main.e1b9924c.chunk.css"
+  },
+  {
+    "revision": "4eef540f08e8a1a45b3c",
+    "url": "./static/js/2.2a1c852e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.2a1c852e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a8bce9338540b21d6557",
+    "url": "./static/js/main.72ca8169.chunk.js"
+  },
+  {
+    "revision": "a146ecc87eabce8758a6",
+    "url": "./static/js/runtime-main.95cf79bc.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  },
+  {
+    "revision": "36297dfde45f69ed485a16ce82cd7f60",
+    "url": "./static/media/temporada1.36297dfd.jpg"
+  },
+  {
+    "revision": "c257dbe9f76ac9ec553720ad51ed7dbd",
+    "url": "./static/media/temporada2.c257dbe9.jpg"
+  }
+]);
